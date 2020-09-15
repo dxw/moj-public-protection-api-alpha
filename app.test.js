6a602e842a471e9b627fd7e882b8a373000e7958
Robbie Paul <robbie.paul@dxw.com>
2020-09-15 16:11:22 +0100

    Fix failing test
    Because this is just a prototype I don't want to spend too much time
    creating useful mocks. Instead just to get the build green we verify
    that the response contains the property we expect.

--- app.test.js
+++ app.test.js
@@ -1,8 +1,13 @@
 const { app } = require("./app");
 const request = require("supertest");
+const Sinon = require('sinon');
+const Review = require('./models/Review');
 
 test("GET /reviews", async () => {
+  Sinon.stub(Review, 'query').returns({
+    withGraphJoined: Sinon.stub().returns(Promise.resolve([{id: 1}]))
+  });
   const response = await request(app).get("/reviews");
   expect(response.status).toBe(200);
-  expect(response.text).toEqual("Hello dxw!");
+  expect(response.body).toHaveProperty("reviews");
 });
